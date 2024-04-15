28aebf709b55ec5146d2a4d2c8f6867f528f13d1
maximelopez <maxime.lpz@outlook.fr>
2024-04-15 09:02:47 +0200

    dotenv et connexion

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
+require('./models/connection');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
