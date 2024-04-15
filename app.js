f050ee0d826dbe0a2caeab4ec026db597f3ee4bb
CamilleWebDev <160153284+CamilleWebDev@users.noreply.github.com>
2024-04-15 15:59:28 +0200

    EventModel

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-require('./models/connection');
+require("./models/connection");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var eventsRouter = require("./routes/events");
 
 var app = express();
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/events", eventsRouter);
 
 module.exports = app;
