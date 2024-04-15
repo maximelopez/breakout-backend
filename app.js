abd095d711edc808c850fde2f320903ee2e08ead
maximelopez <maxime.lpz@outlook.fr>
2024-04-15 10:52:28 +0200

    routes inscription et connexion

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 
-require('./models/connection');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
+const categoriesRouter = require('./routes/categories');
 
 var app = express();
 app.use(cors());
@@ -19,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/categories', categoriesRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,63 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require('../models/connection');
+const User = require('../models/users');
+const bcrypt = require('bcrypt');
+const uid2 = require('uid2');
+
+router.post('/signup', (req, res) => {
+  if (req.body.firstname && req.body.email && req.body.password) {
+
+    // Vérifier si l'utilisateur existe déjà
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        res.json({ result: false, error: 'User already exists' });
+      } else {
+
+        const hash = bcrypt.hashSync(req.body.password, 10);
+
+        const newUser = new User({
+          firstname: req.body.firstname,
+          email: req.body.email,
+          password: hash,
+          token: uid2(32),
+        });
+
+        newUser.save().then(newUser => {
+          res.json({ result: true, token: newUser.token, email: user.email, firstname: user.firstname });
+        });
+        
+      }
+    })
+
+  } else {
+    res.json({ result: false, error: 'Missing or empty fields' });
+  }
+});
+
+
+router.post('/signin', (req, res) => {
+  if (req.body.email && req.body.password) {
+
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        // L'utilisateur existe, vérification du password
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          // Utilisateur connecté
+          res.json({ result: true, token: user.token, email: user.email, firstname: user.firstname });
+        } else {
+          res.json({ result: false, error: 'User not found or wrong password' });
+        }
+
+      } else {
+        res.json({ result: false, error: 'User not found or wrong password' });
+      }
+    })
+
+  } else {
+    res.json({ result: false, error: 'Missing or empty fields' });
+  }
 });
 
 module.exports = router;
