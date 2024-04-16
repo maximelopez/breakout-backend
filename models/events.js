975d7fb5f9b5f226163a60745fe834b74ccdd52c
CamilleWebDev <160153284+CamilleWebDev@users.noreply.github.com>
2024-04-16 09:51:37 +0200

    backend model

--- models/events.js
+++ models/events.js
@@ -4,9 +4,11 @@ const eventSchema = mongoose.Schema({
   title: String,
   date: Date,
   address: String,
-  category: String,
+  category: String, // ou clé étrangère ??
   description: String,
   numberOfSeats: Number,
+  //   participantsList: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  //   eventCreator: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
 });
 
 const Event = mongoose.model("events", eventSchema);
