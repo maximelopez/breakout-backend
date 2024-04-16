18af40644e1ad05ad507d2c42c16fca81c089eef
maximelopez <maxime.lpz@outlook.fr>
2024-04-16 14:29:24 +0200

    routes events

--- models/events.js
+++ models/events.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const eventSchema = mongoose.Schema({
   title: String,
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'users'},
   date: Date,
   address: String,
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'categories'},
   description: String,
   numberOfSeats: Number,
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users'}],
 });
 
 const Event = mongoose.model("events", eventSchema);
