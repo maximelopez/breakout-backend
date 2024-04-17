66fbc467c66be6286153e752c3a5637ade0c57fc
CamilleWebDev <160153284+CamilleWebDev@users.noreply.github.com>
2024-04-17 10:11:16 +0200

    backend

--- models/events.js
+++ models/events.js
@@ -7,13 +7,8 @@ const eventSchema = mongoose.Schema({
   address: String,
   category: { type: mongoose.Schema.Types.ObjectId, ref: "categories" },
   description: String,
-<<<<<<< HEAD
-  numberOfSeats: Number,
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
-=======
   seats: Number,
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users'}],
->>>>>>> 6199bfccb217f7f2415d0c8c9e5dcf16989d0c6b
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
 });
 
 const Event = mongoose.model("events", eventSchema);
