f74867661571921075155c728f20385aa4110ecb
CamilleWebDev <160153284+CamilleWebDev@users.noreply.github.com>
2024-04-23 09:53:09 +0200

    petits changements route suppression user

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const usersCtrl = require("../controllers/users");
 
 router.post("/signup", usersCtrl.signup);
 router.post("/signin", usersCtrl.signin);
-router.delete("/remove", usersCtrl.remove);
+router.delete("/remove/:token", usersCtrl.remove);
 router.put("/modify/:token", usersCtrl.modify);
 router.get("/print/:token", usersCtrl.print);
 module.exports = router;
